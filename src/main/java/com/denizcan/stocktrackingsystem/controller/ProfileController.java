2aff00e11c93acd47b9434ee6c32d27a51adc5e9
Deniz Can Demir <denizcandemir94@gmail.com>
2025-05-01 12:29:54 +0300

    Login and Register update

--- src/main/java/com/denizcan/stocktrackingsystem/controller/ProfileController.java
+++ src/main/java/com/denizcan/stocktrackingsystem/controller/ProfileController.java
@@ -2,7 +2,6 @@ package com.denizcan.stocktrackingsystem.controller;
 
 import com.denizcan.stocktrackingsystem.model.User;
 import com.denizcan.stocktrackingsystem.repository.UserRepository;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
