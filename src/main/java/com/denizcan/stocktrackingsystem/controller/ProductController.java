c3e026ec0f49f27e0f1af7a6fdded0bc795f03fa
Deniz Can Demir <denizcandemir94@gmail.com>
2025-05-03 16:30:30 +0300

    Lat version of 3.5.2025

--- src/main/java/com/denizcan/stocktrackingsystem/controller/ProductController.java
+++ src/main/java/com/denizcan/stocktrackingsystem/controller/ProductController.java
@@ -11,7 +11,6 @@ import org.springframework.ui.Model;
 import java.util.List;
 import java.util.Optional;
 import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 @Controller
 public class ProductController {
